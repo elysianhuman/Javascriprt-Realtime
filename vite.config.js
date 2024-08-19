e211ce1257bbd7239ec7f0c71199f649489e3fbb
admin1 <admin1@focus.in>
2024-08-19 14:53:09 +0530

    fixed blank page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 import prettier from "vite-plugin-prettier";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), prettier()],
+//   base: "/Javascriprt-Realtime/"
+// });
+
 export default defineConfig({
   plugins: [react(), prettier()],
-  base: "/Javascriprt-Realtime/"
+  base: "./"
 });
